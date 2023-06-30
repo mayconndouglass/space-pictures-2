aa68fb2808d2fea6005a16121a4071b41562b406
mayconndouglass <mayconn.douglass1414@gmail.com>
2023-06-30 17:43:02 -0300

    Implemented app_routes, changes in nasaCard and space_pictures

--- lib/app/space_pictures.dart
+++ lib/app/space_pictures.dart
@@ -1,24 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:space_pictures_2/models/nasa_data.dart';
+import 'package:space_pictures_2/pages/details_page.dart';
 
 import '../pages/home_page.dart';
 import '../providers/nasa_data_provider.dart';
+import '../routes/app_route.dart';
 
 class SpacePicturesApp extends StatelessWidget {
   const SpacePicturesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => NasaDataProvider(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Space Pictures',
-        theme: ThemeData(
-        colorSchemeSeed: Colors.black87,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Space Pictures',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-        home: const HomePage(),
+      home: ChangeNotifierProvider(
+        create: (context) => NasaDataProvider(),
+        child: const HomePage(),
       ),
+      routes: {
+        AppRoute.details.name: (context) {
+          final data = ModalRoute.of(context)?.settings.arguments as NasaData?;
+          return DetailsPage(
+            data: data!,
+          );
+        },
+      },
     );
   }
 }
--- lib/widgets/nasa_card.dart
+++ lib/widgets/nasa_card.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:space_pictures_2/models/nasa_data.dart';
-
-import '../pages/details_page.dart';
+import '../routes/app_route.dart';
 
 class NasaCard extends StatelessWidget {
   final NasaData data;
 
-  const NasaCard({super.key, required this.data});
+  const NasaCard({Key? key, required this.data}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 16.0,
-      margin: const EdgeInsets.all(12.0),
+      margin: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 16.0),
       color: Colors.grey,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(12.0),
@@ -21,44 +20,45 @@ class NasaCard extends StatelessWidget {
         children: [
           Stack(
             children: [
-              const SizedBox(
-                /* width: 400, */
-                height: 300,
+              const Positioned.fill(
                 child: Center(
                   child: CircularProgressIndicator(
                     valueColor: AlwaysStoppedAnimation(Colors.blue),
                   ),
                 ),
               ),
+              SizedBox(
+                height: 250,
+                width: 500,
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.vertical(bottom: Radius.zero, top: Radius.circular(12.0)),
+                  child: Image.network(
+                    data.url,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
               Hero(
                 tag: data.date,
-                child: InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => DetailsPage(data: data),
-                      ),
-                    );
-                  },
-                  child: SizedBox(
-                    height: 300,
-                    width: 500,
-                    child: ClipRRect(
-                      borderRadius: const BorderRadius.vertical(bottom: Radius.zero, top: Radius.circular(12.0)),
-                      child: Image.network(
-                        data.url,
-                        fit: BoxFit.cover,
-                      ),
+                child: Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.pushNamed(context, AppRoute.details.name, arguments: data);
+                    },
+                    child: const SizedBox(
+                      height: 250,
+                      width: 500,
+                      child: SizedBox(), // Ocupa espaço para o InkWell funcionar
                     ),
                   ),
                 ),
-              )
+              ),
             ],
           ),
-          Padding(padding: const EdgeInsets.symmetric(horizontal: 10.0),
-            child: 
-            Text(
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Text(
               data.title,
               style: const TextStyle(
                 fontSize: 16.0,
@@ -66,15 +66,16 @@ class NasaCard extends StatelessWidget {
               ),
             ),
           ),
-          Padding(padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
             child: Text(
-            data.date,
-            style: const TextStyle(
-              fontSize: 16.0,
-              fontWeight: FontWeight.bold,
+              data.date,
+              style: const TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          )
-          )
+          ),
         ],
       ),
     );
